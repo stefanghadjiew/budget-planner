0b26c3cbef18446ca8782aaadd5328a66d338efb
Stefan G. Hadzhiev <stefan.hadzhiev@musala.com>
2023-05-06 23:19:15 +0300

    feature: Add DB and db configuration

---
+++ srv/config.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MONGO_COLLECITON_NAME = exports.MONGO_CLUSTER = exports.MONGO_PASSWORD = exports.MONGO_USER = void 0;
+exports.MONGO_USER = process.env.MONGO_USER;
+exports.MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+exports.MONGO_CLUSTER = process.env.MONGO_CLUSTER;
+exports.MONGO_COLLECITON_NAME = process.env.MONGO_COLLECITON_NAME;
---
+++ srv/config.ts
@@ -0,0 +1,4 @@
+export const { MONGO_USER } = process.env;
+export const { MONGO_PASSWORD } = process.env;
+export const { MONGO_CLUSTER } = process.env;
+export const { MONGO_COLLECITON_NAME } = process.env;
