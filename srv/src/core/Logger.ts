1d51da9b18cf34e015c0c59e1bf57a499c17b777
Stefan G. Hadzhiev <cheffo0odev@abv.bg>
2023-06-02 00:56:31 +0300

    feature: add custom Logger class

---
+++ srv/src/core/Logger.ts
@@ -0,0 +1,22 @@
+/* eslint-disable implicit-arrow-linebreak */
+import chalk from 'chalk';
+
+export default class Logger {
+  public static info = (args: any) =>
+    console.log(
+      chalk.blue(`[${new Date().toLocaleString()}] [INFO] `),
+      typeof args === 'string' ? chalk.blueBright(args) : args,
+    );
+
+  public static warning = (args: any) =>
+    console.log(
+      chalk.yellow(`[${new Date().toLocaleString()}] [INFO] `),
+      typeof args === 'string' ? chalk.yellowBright(args) : args,
+    );
+
+  public static error = (args: any) =>
+    console.log(
+      chalk.red(`[${new Date().toLocaleString()}] [INFO]`),
+      typeof args === 'string' ? chalk.redBright(args) : args,
+    );
+}
