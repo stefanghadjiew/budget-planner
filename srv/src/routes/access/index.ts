26ad1d5e2cb0b7076120d49b2db80362973a3da5
Stefan Hadzhiev <cheffo0odev@abv.bg>
2023-06-19 23:31:36 +0300

    feature: add signup route

--- srv/src/routes/access/index.ts
+++ srv/src/routes/access/index.ts
@@ -4,5 +4,7 @@ import signup from './signup';
 // eslint-disable-next-line import/prefer-default-export
 const accessRouter = express.Router();
 accessRouter.use('/signup', signup);
+/* accessRouter.use('/login', login);
+accessRouter.use('/logout', logout); */
 
 export default accessRouter;
